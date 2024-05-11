1faa198a55bb70de5709b0fa8b8b4d206541554e
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2024-05-11 16:57:24 +0800

    install homebrew
    and comment out nix, pyenv, php-cli

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -5,28 +5,38 @@ sudo apt install zsh
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/davidparsson/zsh-pyenv-lazy.git ~/.oh-my-zsh/custom/plugins/pyenv-lazy
 
 # tmux
 sudo apt install tmux
 
-# Nix
-curl -L https://nixos.org/nix/install | sh
-
-nix-env -iA nixpkgs.diff-so-fancy \
-            nixpkgs.silver-searcher \
-            nixpkgs.ripgrep \
-            nixpkgs.fd \
-            ;
-
 # nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-nvm install 14
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+nvm install 20
+
+# Homebrew on Linux
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install \
+    diff-so-fancy \
+    the_silver_searcher \
+    ripgrep \
+    fd \
+    ;
+
+# Nix
+# curl -L https://nixos.org/nix/install | sh
+#
+# nix-env -iA nixpkgs.diff-so-fancy \
+#             nixpkgs.silver-searcher \
+#             nixpkgs.ripgrep \
+#             nixpkgs.fd \
+#             ;
+
 
 # PHP
-sudo apt install php-cli
+#sudo apt install php-cli
 
 # pyenv
-curl https://pyenv.run | bash
+#curl https://pyenv.run | bash
 
 
